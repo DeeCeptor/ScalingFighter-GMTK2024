606aa054b04025fe4eef525098b68e3c5067dd46
DeeCeptor <sirmichaellong@.com>
2024-08-16 23:55:18 -0600

    VN engine import
    AnimationManager

---
+++ ScalingFighterUnity/Assets/Scripts/AnimationManager.cs
@@ -0,0 +1,117 @@
+using System;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+using UnityEngine.UI;
+using System.Linq;
+
+public class AnimationManager : MonoBehaviour
+{
+    public static AnimationManager instance;
+    /// <summary>
+    /// DEBUG, view in-editor which animation we're on (in case we get stuck)
+    /// </summary>
+    [TextArea]
+    public string CurrentAnimationName;
+
+    /// <summary>
+    /// Animations are COROUTINES
+    /// </summary>
+    Tuple<IEnumerator, bool> current_animation;
+    /// <summary>
+    /// Animations WAITING to be played
+    /// <COROUTINE, BLOCKS SCENE CONTROLS>
+    /// </summary>
+    public Queue<Tuple<IEnumerator, bool>> queued_animations = new Queue<Tuple<IEnumerator, bool>>();
+
+    /// <summary>
+    /// If game over, only accept certain new animations
+    /// </summary>
+    public bool accepting_normal_animations = true;
+    IEnumerator AnimationLoopCo;
+
+
+    IEnumerator PlayAnimationsLoop()
+    {
+        while (true)
+        {
+            while (queued_animations.Count > 0)// || VNSceneManager.current_conversation != null)
+            {
+                /*
+                if (VNSceneManager.current_conversation != null)
+                {
+                    SceneParent.SetSceneInteractable(false);    // No interactivity during conversations
+                }
+                else
+                {
+                */
+                current_animation = queued_animations.Dequeue();
+                //  Set if scene is clickable or not
+                SceneParent.SetSceneInteractable(!current_animation.Item2);
+                //Debug.Log("Starting animation: " + current_animation);
+                // Wait until animation finishes
+                if (current_animation.Item1 != null)
+                {
+                    CurrentAnimationName = current_animation.Item1.ToString();
+                    yield return StartCoroutine(current_animation.Item1);
+                }
+                else
+                    Debug.LogError("null animation");
+                CurrentAnimationName = "";
+                current_animation = null;
+                yield return null;
+            }
+            CurrentAnimationName = "";
+            SceneParent.SetSceneInteractable(true);
+            yield return null;
+        }
+    }
+
+    public static void AddAnim(IEnumerator anim, bool override_dont_accept_animations = false, bool blocks_controls = true)
+    {
+        AnimationManager.instance.AddAnimation(anim, override_dont_accept_animations: override_dont_accept_animations, blocks_controls: blocks_controls);
+    }
+    public void AddAnimation(IEnumerator anim, bool override_dont_accept_animations = false, bool blocks_controls = true)
+    {
+        if (!accepting_normal_animations && !override_dont_accept_animations)
+            return;
+        queued_animations.Enqueue(new Tuple<IEnumerator, bool>(anim, blocks_controls));
+    }
+    public static void ClearALLQueuedAnimations()
+    {
+        AnimationManager.instance.queued_animations.Clear();
+        // Turn OFF then ON this object to kill coroutine
+        AnimationManager.instance.RestartAnimationsLoop();
+    }
+
+    void Awake()
+    {
+        instance = this;
+    }
+
+    private void OnEnable()
+    {
+        RestartAnimationsLoop();
+    }
+    public void RestartAnimationsLoop()
+    {
+        if (AnimationLoopCo != null)
+            StopCoroutine(AnimationLoopCo);
+        AnimationLoopCo = PlayAnimationsLoop();
+        StartCoroutine(AnimationLoopCo);
+    }
+
+
+    /// <summary>
+    /// Add to animation queue for prompt
+    /// </summary>
+    /// <param name="cm"></param>
+    /// <returns></returns>
+    public static IEnumerator StartConversationPrompt(ConversationManager cm)
+    {
+        yield return null;
+        if (cm != null)
+            cm.Start_Conversation();
+    }
+}
---
+++ ScalingFighterUnity/Assets/Scripts/SceneParent.cs
@@ -0,0 +1,96 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class SceneParent : MonoBehaviour
+{
+    public static SceneParent scene_parent;
+    /// <summary>
+    /// FALSE, user can't click on anything
+    /// </summary>
+    public static bool scene_is_interactable
+    {
+        get { return SceneParent.scene_parent.group.interactable; }// && CameraShake.camera_shake.shake <= 0f; }
+    }
+    public CanvasGroup group;
+    public CanvasGroup ui_group_to_fade;
+    public Canvas main_canvas;
+
+    public GameObject click_blocker;
+    public bool debug_override_ui_visible = true;
+    public bool ui_visible = true;
+    public bool scene_interactactable {
+        get { return scene_is_interactable; }
+    }
+    float min_alpha = 0.001f;
+
+    void Awake()
+    {
+        scene_parent = this;
+        if (group == null)
+            group = this.GetComponent<CanvasGroup>();
+    }
+
+
+    public static void SetSceneInteractable(bool interactable)
+    {
+        SceneParent.scene_parent.group.interactable = interactable;
+        SceneParent.scene_parent.group.blocksRaycasts = interactable;
+        if (scene_parent != null && scene_parent.click_blocker != null)
+        {
+            scene_parent.click_blocker.SetActive(!interactable);
+        }
+    }
+
+    public void OverrideDebugToggleUI()
+    {
+        debug_override_ui_visible = !debug_override_ui_visible;
+        ShowHideUI(debug_override_ui_visible);
+    }
+    public void ToggleUI()
+    {
+        ShowHideUI(!ui_visible);
+    }
+    public void ShowHideUI(bool show)
+    {
+        if (!debug_override_ui_visible)
+            show = false;
+        ui_visible = show;
+        //Debug.Log("ShowHideUI " + show);
+    }
+    public void ShowUI()
+    {
+        ShowHideUI(true);
+    }
+    public void HideUI()
+    {
+        ShowHideUI(false);
+    }
+
+
+    void Update()
+    {
+        /*
+        // Changing is SUPER LAGGY. Fix this!
+        if (ui_visible && ui_group_to_fade.alpha != 1f)
+        {
+            ui_group_to_fade.alpha = 1f;
+        }
+        else if (!ui_visible && ui_group_to_fade.alpha != 0f)
+        {
+            ui_group_to_fade.alpha = 0f;
+        }
+        */
+        if (ui_group_to_fade != null)
+        {
+            if (ui_visible && ui_group_to_fade.alpha != 1f)
+            {
+                ui_group_to_fade.alpha = Mathf.MoveTowards(ui_group_to_fade.alpha, 1f, Time.deltaTime);
+            }
+            else if (!ui_visible && ui_group_to_fade.alpha != 0f)
+            {
+                ui_group_to_fade.alpha = Mathf.MoveTowards(ui_group_to_fade.alpha, 0f, Time.deltaTime);
+            }
+        }
+    }
+}
